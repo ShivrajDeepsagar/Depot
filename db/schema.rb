eda00d3be0117a139260c1dd18dc0c8bdef33a95
ShivrajDeepsagar <shivaraj.deepasagar@gmail.com>
2017-10-01 20:45:05 +0530

    scaffold product creation and validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728145455) do
+ActiveRecord::Schema.define(version: 20170831182758) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
